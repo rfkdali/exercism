093c34e9c1de4aea4a46f3ea494a1eeeef179b14
rfkdali <rfkdali@gmail.com>
2017-02-27 11:06:48 +0100

    Raindrops: Factors are constants, so use a constant

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,14 +1,16 @@
 class Raindrops
+
+  FACTORS = {
+    3 => "Pling",
+    5 => "Plang",
+    7 => "Plong"
+  }
+
   def self.convert number
-    factors = {
-      3 => "Pling",
-      5 => "Plang",
-      7 => "Plong"
-    }
     result = []
         
-    factors.keys.each do |factor|
-      result << factors[factor] if number % factor == 0
+    FACTORS.keys.each do |factor|
+      result << FACTORS[factor] if number % factor == 0
     end
     
     result.empty? ? number.to_s : result.join
